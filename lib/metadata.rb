d4b843d6dffbe264bbd37decb981ed39f19dca1e
DEkart <dekart@Lamptop.(none)>
2008-08-02 15:45:39 +0600

    Javascripts and stylesheets are now separate classes

--- lib/metadata.rb
+++ lib/metadata.rb
@@ -1,8 +1,42 @@
 module Metadata
-  class Base
+  class Stylesheet
     include ERB::Util
     include ActionView::Helpers::TagHelper
     include ActionView::Helpers::AssetTagHelper
+
+    def initialize(*options)
+      @value = options
+    end
+    
+    def options
+      @value.dup.extract_options!
+    end
+    
+    def to_html
+      html = stylesheet_link_tag(*@value)
+      
+      return "<!--[if ie]>#{html}<![endif]-->" if options[:browser] == :ie
+      return javascript_tag("if(window.opera){document.write('#{html}')}") if options[:browser] == :opera
+    end
+  end
+  
+  class Javascript
+    include ERB::Util
+    include ActionView::Helpers::TagHelper
+    include ActionView::Helpers::AssetTagHelper
+    
+    def initialize(*options)
+      @value = options
+    end
+    
+    def to_html
+      return javascript_tag(@value)
+    end
+  end
+  
+  class Base
+    include ERB::Util
+    include ActionView::Helpers::TagHelper
     
     DEFAULT_OPTIONS = {
       :join => " - "
@@ -22,6 +56,14 @@ module Metadata
       @options = DEFAULT_OPTIONS.merge(options)
     end
     
+    def stylesheets=(values)
+      @stylesheets = values.collect {|value| Stylesheet.new(value) }
+    end
+    
+    def javascripts=(values)
+      @stylesheets = values.collect {|value| Stylesheet.new(value) }
+    end
+
     def merge(other_meta)
       other_meta = self.class.new(other_meta) if other_meta.kind_of?(Hash)
       
@@ -39,13 +81,14 @@ module Metadata
         html << tag(:meta, :name => :keywords, :content => @keywords * @options[:join]) if @keywords.any?
         
         @stylesheets.each do |stylesheet|
-          html << stylesheet_link_tag(*stylesheet)
+          html << stylesheet.to_html
         end
         
         @javascripts.each do |javascript|
-          html << javascript_include_tag(*javascript)
+          html << javascript.to_html
         end
       end
     end
+    
   end
 end
